109883d01e47b175743188e63af1a0b751c7219d
Becky75 <alpaca.love5@gmail.com>
2022-07-20 14:11:03 -0700

    updated

---
+++ project4_dictionaries/js/main.js
@@ -0,0 +1,11 @@
+function my_dictionaries() {
+    var animal = {
+        species: "Dog",
+        color: "Black",
+        Breed: "labrador",
+        Age: 5,
+        Sound: "bark!"
+    };
+    document.getElementById("dictionaries").innerHTML = Animal.sound;
+    )
+}
