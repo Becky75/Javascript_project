000e653e8e285b059e2bb70d719798b3826442a4
Becky75 <alpaca.love5@gmail.com>
2022-07-21 14:00:34 -0700

    update

--- project5_comparisons_type_coercion/js/main.js
+++ project5_comparisons_type_coercion/js/main.js
@@ -1,9 +1,46 @@
+document.write(typeof 3);
+
 function my_function() {
-    document.write(Typeof "word");
-
+   
     document.getElementById("test").innerHTML = 0 / 0;
+}//this function uses typeof and returns nan
 
-    document.write(12 > 10);//output should be true
+// displays infinity and negative infinty
+    document.write(2E310);
+    DocumentFragment.write(-3E310);
 
-    document.write(12 < 10);//output shuold be false
+//math operators return less and greater than
+    document.write(12 > 10);//less than
+    document.write(12 < 10);//greater than
+
+    //these are console logs
+    console.log(2 + 2);
+    console.log(12>6);
+
+//type coercion attempts
+document.write(10 + 5);
+document.write(10 == 10);
+document.write(3 == 11);
+
+//trying double equal signs
+x = 10;
+y = 10;
+document.write(x === y);
+
+z = 25;
+w = 25;
+document.write(z === w);
+
+//local operators
+document.write(9 > 2 && 10 > 2);
+document.write(6 > 12 && 6 > 2);
+document.write(7 > 10 || 10 > 8);
+document.write(7 > 10 || 10 > 12);
+
+function not_function() {
+    document.getElementsById("not").innerHTML = !(20 > 10);
+}
+
+function not_function() {
+    document.getElementById("not2").innerHTML = !(15 > 20);
 }
