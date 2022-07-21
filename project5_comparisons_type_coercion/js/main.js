6fd1f32b55f57fdfdb01253881142688fd4f1300
Becky75 <alpaca.love5@gmail.com>
2022-07-21 12:32:12 -0700

    update

--- project5_comparisons_type_coercion/js/main.js
+++ project5_comparisons_type_coercion/js/main.js
@@ -1,4 +1,9 @@
-document.write(Typeof "word")
 function my_function() {
-    document.getElementById("test").innerHTML = 0/0;
+    document.write(Typeof "word");
+
+    document.getElementById("test").innerHTML = 0 / 0;
+
+    document.write(12 > 10);//output should be true
+
+    document.write(12 < 10);//output shuold be false
 }
