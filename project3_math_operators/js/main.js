86981583e86d1926e1badc4c821dd45c82a1c33e
Becky75 <alpaca.love5@gmail.com>
2022-07-20 19:30:19 -0700

    updated

--- project3_math_operators/js/main.js
+++ project3_math_operators/js/main.js
@@ -1,7 +1,7 @@
 function addition_function() {
     //this is the aquation
     var addition = 4 + 6;
-    document.getElementById("math").innerHTML = "4+6=" + addition;
+    document.getElementById("add").innerHTML = "4+6=" + addition;
 }
 function substraction_function() {
     //this is the aquation
@@ -22,4 +22,8 @@ function modulus_operator() {
     var simple_Math = 25 % 6;
     document.getElementById("modulus").innerHTML = "When you divide 25 by 6 you have a remainder of:" + simple_Math;
 
+}
+function negation_operator() {
+    var x = 10;
+    document.getElementById("Math").innerHTML = -x;
 }
