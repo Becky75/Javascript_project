6dfd8e6e6d9467503caa57c4936b561ee11fbe73
Becky75 <alpaca.love5@gmail.com>
2022-07-24 12:05:18 -0700

    update
    wont display function 2.  has binary code at top of page after adding in lines 12 to  39

---
+++ Project7_scope-time_function/Js/main.js
@@ -0,0 +1,39 @@
+myFunction();
+//code here can use horsename
+function myFunction() {
+   var horsename = "Harley";
+}
+
+//code here can use horsename
+function myFunction2() {
+    myFunction2();
+    var horsename2 = "Jax";
+}
+//to show global variables
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + x + ",<br>");
+}
+function Add_numbers2() {
+    document.write(x + 100);
+}
+Add_numbers_1();
+Add_numbers2();
+
+//to show local variables
+function Add_numbers_1() {
+    var x = 10;
+    document.write(20 + x + ",<br>");
+}
+function Add_numbers2() {
+    document.write(x + 100);
+}
+Add_numbers_1();
+Add_numbers2();
+//this is for the console log
+function Add_numbers_1(){
+    var x = 10;
+    console.log(x +100);
+}
+Add_numbers_1();
+Add_numbers2();
