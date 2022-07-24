31055f6b00a0f214e1c3a4e87d22d182fa576c08
Becky75 <alpaca.love5@gmail.com>
2022-07-24 10:38:14 -0700

    update

--- project6_tenary_operators_constructors/js/main2.js
+++ project6_tenary_operators_constructors/js/main2.js
@@ -1,8 +1,10 @@
-function count_Function() {
+function Function() {
     document.getElementById("counting").innerHTML = Count();
     function Count() {
-        var starting_point = 9;
-        function Plus_one() { starting_point = 1; }
+        var starting_point = 1;
+        function Plus_one() {
+            starting_point += 1;
+        }
         Plus_one();
         return starting_point;
     }
