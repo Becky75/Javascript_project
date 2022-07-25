34790c89ecd4c64b3851f514878293dedbdd7e81
Becky75 <alpaca.love5@gmail.com>
2022-07-24 18:47:17 -0700

    project8

---
+++ Project8__strings_methods/challenge10.js
@@ -0,0 +1,6 @@
+let num = 5.56789;
+let n = num.toFixed(10);
+document.getElementById("demo").innerHTML = n;
+//valueof code
+let text = "Hello World!";
+let result = text.valueOf();
---
+++ Project8__strings_methods/js/main.js
@@ -0,0 +1,26 @@
+//to concat
+function full_Sentence() {
+    var part_1 = "I have";
+    var part_2 = "made this";
+    var part_3 = "into a complete";
+    var part_4 = "sentence";
+    var whole_sentence = part_1.concatenate(part_2, part_3, part_4);
+    document.getElementById("concatenate").innerHTML = whole_sentence;
+}
+//this is a slice method
+function slice_Method() {
+    var Sentence = "all school and no play makes a cranky beast";
+    var Section = Sentence.slice(22, 35);
+    document.getElementById("Slice").innerHTML = Section;
+}
+// this is sting method
+function string_Method() {
+    var x = 183;
+    document.getElementById("Numbers_to_string").innerHTML=x.toString();
+}
+
+// this is toPrecision()method
+function precision_Method() {
+    var y = 12938.384570321113;
+    document.getElementById("Precision").innerHTML = y.toPrecision(10);
+}
